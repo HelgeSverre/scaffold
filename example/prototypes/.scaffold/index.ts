c4fd22c38c7669d4524e5b2e4e1387cc19141b51
Helge Sverre <helge.sverre@gmail.com>
2026-02-13 00:46:54 +0100

    example: reorganize prototypes into .scaffold structure

--- example/prototypes/.scaffold/index.ts
+++ example/prototypes/.scaffold/index.ts
@@ -1,6 +1,7 @@
 import { startServer } from "scaffold";
+import { join } from "path";
 
 startServer({
-  dir: import.meta.dir,
+  dir: join(import.meta.dir, ".."),
   port: Number(process.env.PORT) || 1234,
 });
